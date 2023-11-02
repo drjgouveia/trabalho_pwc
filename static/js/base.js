5a0bd4d9cdfc56dc8ef7fe3fc3502531b1f45dc6
Diogo Gouveia <diogo.gouveia2@gmail.com>
2023-11-02 10:24:09 +0000

    Alterações a nome da pasta, fiz com que o header seja importado através de requests

---
+++ static/js/base.js
@@ -0,0 +1,15 @@
+window.onload = function () {
+  const headerContainer = document.getElementById("header");
+  const footerContainer = document.getElementById("footer");
+
+  fetch("components/header.html")
+    .then((response) => response.text())
+    .then((data) => {
+      headerContainer.innerHTML = data;
+    });
+  fetch("components/footer.html")
+    .then((response) => response.text())
+    .then((data) => {
+      footerContainer.innerHTML = data;
+    });
+};
