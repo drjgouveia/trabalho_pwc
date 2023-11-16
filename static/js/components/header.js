f5391915ca78dc95c77b38c6e6f7d56a26dd7cb3
imrafaelreis <2231432@my.ipleiria.pt>
2023-11-16 21:55:59 +0000

    Adicionamento de tudo

--- static/js/components/header.js
+++ static/js/components/header.js
@@ -1,5 +1,6 @@
 function definirActiveClassHeader() {
-  const path = window.location.pathname.slice(1);
+  let path = window.location.pathname.slice(1);
+  path = path.split("/")[path.split("/").length - 1];
   const links = document.querySelectorAll(".nav-link");
   links.forEach((link) => {
     if (link.getAttribute("href") === path) {
